e24781c84148dd0823c851fb38052d9f8e0100a6
Fe-Ti-Cl <79993359+Fe-Ti-Cl@users.noreply.github.com>
2023-10-04 15:01:46 +0300

    Update docker_start.sh

--- docker_start.sh
+++ docker_start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
-[ -z "$CONTAINER_NAME" ] && CONTAINER_NAME=docker.io/Fe-Ti/iscraweb-ttbot:latest
+die() {
+    echo "$1"; exit 1
+}
+[ -z "$CONTAINER_NAME" ] && die 'CONTAINER_NAME is not set! Set envvar $CONTAINER_NAME'
 
 runner_args=(run --rm -v $(git rev-parse --show-toplevel)/config.json.tmpl:/config.json.tmpl -e REDMINE_URL=$REDMINE_URL -e CFG=/config.json.tmpl -e K=$K -e T=$T "$CONTAINER_NAME")
 
